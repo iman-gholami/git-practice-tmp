c560790de131c115fc352cc7d7a33f8a878d9bbd
iman-gholami <iman.noc1998@gmail.com>
2023-08-29 20:11:22 +0430

    Add new Api with Get Metod

--- index.js
+++ index.js
@@ -6,10 +6,14 @@ const app = express();
 const PORT = 9300 ;
 
 
-app.get('/home' , (req , res)=>{
+app.get('/' , (req , res)=>{
     res.send('hello World')
 })
 
+
+app.('/home', (req, res)=>{
+    res,send('Wellcome To the Home Page Here')
+})
 
 
 app.listen(PORT , ()=>{
