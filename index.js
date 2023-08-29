ea8a5a8483529321a60edc4f31e000f08eb9f8de
iman-gholami <iman.noc1998@gmail.com>
2023-08-29 08:41:33 +0430

    get api

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ const app = express();
 const PORT = 9300 ;
 
 
+app.get('/home' , (req , res)=>{
+    res.send('hello World')
+})
+
+
 
 app.listen(PORT , ()=>{
     console.log(`server listening on ${PORT}`);
 });
+
